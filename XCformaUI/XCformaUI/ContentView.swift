906cb56f90d4b05637f19e8da8f497e68382c2ee
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-01-17 09:06:32 +0100

    Companion app

---
+++ XCformaUI/XCformaUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCformaUI
+//
+//  Created by Sebastian Gfeller on 16.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("XCUITest for MAUI")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCformaUI/XCformaUI/XCformaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCformaUIApp.swift
+//  XCformaUI
+//
+//  Created by Sebastian Gfeller on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct XCformaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
