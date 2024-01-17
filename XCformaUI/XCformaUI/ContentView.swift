37694632d8cf4f026e8179452fce3fd6f3ba9953
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-01-17 14:31:51 +0100

    Adding companion app

---
+++ XCformaUI/XCformaUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCformaUI
+//
+//  Created by Sebastian Gfeller on 17.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Companion app for testing")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
